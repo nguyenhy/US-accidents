0588da82c3ed8480e1e6a414737b8d6fbbb087ae
nguyenhy <nguyenhy311@gmail.com>
2024-01-21 16:46:25 +0700

    feat(config): setup config, shell script to packing repo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='us-accidents',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
